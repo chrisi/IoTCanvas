4e313d59a73cb3e697c0801439c9fe2877276d94
Christian Gebauer <mail@christiangebauer.de>
2015-10-18 23:33:02 +0200

    Initial Commit

---
+++ IoTCanvas/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace IoTCanvas
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            var a = new Vector2(10, 10);
+            var b = new Vector2(100, 100);
+
+            args.DrawingSession.DrawLine(a, b, Colors.Yellow);
+        }
+    }
+}
